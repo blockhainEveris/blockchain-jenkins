f7b715630f6e9ceeba30a40614f162d10b39e05e
Dani Peris <dperisro@everis.com>
2017-05-15 23:27:21 +0200

    External api URL

---
+++ config.ts
@@ -0,0 +1,4 @@
+export var config = {
+  query: "https://blockchain-middleware-everis.herokuapp.com/api/v1/apps/blockchain/query",
+  vote: "https://blockchain-middleware-everis.herokuapp.com/api/v1/apps/blockchain/vote"
+}
